80e26960dfd36cc221a73e13a54ccffe7c887023
shaban15 <shingorashaban@gmail.com>
2024-07-11 10:00:01 +0300

    commit001

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>BOM</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="sec">
+      <div class="banner"><marquee direction="left">Welcome, browse get your preferred item and place your orders</marquee></div>
+    </section>
+    <section>
+        <div class="header">
+          <div class="bom"><h1>Banshe Online Market</h1></div>
+          <div class="contact">
+            Contact us<br>Tel: +254 768 553 865<br>email: bomarket@gmail.com<br>Facebook:<a href="https://www.facebook.com/profile.php?id=100079780899412"> BansheOM</a> <br>instagram:<a href=https://www.instagram.com/direct/inbox>banshe_onlinemarket</a> <br>twitter: bansheOmarket
+          </div>
+        </div>
+    </section>
---
+++ includes/navbar.php
@@ -0,0 +1,55 @@
+<link rel="stylesheet" href="navbarstyle.css">
+<section name="sec1">
+        <div class="one">
+            <div class="flyout">
+                <ul type="none">
+                    <li>Smartphones</li>
+                    <li>Computing</li>
+                    <li>Fashion</li>
+                    <li>Sporting and games</li>
+                    <li>Fast foods</li>
+                    <li>Groceries</li>
+                    <li>Office</li>
+                    <li>Appliances</li>
+                    <li>Furniture</li>
+                </ul>
+            </div>
+            <div class="two"></div>
+            <div class="help">
+                <h3><a href="e-form.html">Need any help?</a></h3><br>
+                <h3>Return Items?</h3>
+                <br>
+                <br>
+                <h3>Sell on BoM</h3><br>
+                <h3>Other</h3>
+            </div>
+        </div>
+    </section> 
+    <section class="surplass">
+        <div class="smartphones">Smartphones
+            <a href="https://www.jumia.co.ke/phones-tablets/"> <button>Check products</button></a>
+        </div>
+        <div class="computing">Computing equipment</div>
+        <div class="fashion">Fashion</div>
+        <div class="appliances">Appliances</div>
+        <div class="groceries">Grocer</div>
+        <div class="sporting">Sporting and games</div>
+        <div class="fastfoods">Fast foods</div>
+        <div class="office">Office</div>
+        <div class="furniture">Others</div>
+    </section>
+    <section class="otherlikes">
+        <div class="div1"></div>
+        <div class="div2"></div>
+        <div class="div3"></div>
+        <div class="div4"></div>
+        <div class="div5"></div>
+        <div class="div6"></div>
+        <div class="div7"></div>
+        <div class="div8"></div>
+        <div class="div9"></div>
+        <div class="div10"></div>
+        <div class="div11"></div>
+        <div class="div12"></div>
+    </section>
+   
